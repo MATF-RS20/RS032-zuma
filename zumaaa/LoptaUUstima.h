1f60d4fdca977cfb7a1b37bdf0887657c78e6bad
Katarina Savičić <44586197+savicic@users.noreply.github.com>
2019-12-11 23:44:42 +0100

    Update LoptaUUstima.h

--- zumaaa/LoptaUUstima.h
+++ zumaaa/LoptaUUstima.h
@@ -3,14 +3,26 @@
 
 #include <QGraphicsRectItem>
 #include <QObject>
+#include <QEvent>
+#include <QMouseEvent>
 
 //da bi mogli slotovi da se koriste mora klasa da nasledjuje QObject i da ima ovaj Q_Object makro na pocetku klase
-class LoptaUUstima: public QObject, public QGraphicsRectItem{
+class LoptaUUstima: public QObject, public QGraphicsEllipseItem{
         Q_OBJECT
 public:
     LoptaUUstima();
+
+    // Seter za a i b
+    void postaviAB(double a, double b);
 public slots:
     void move();
+
+private:
+
+    // Privatne promenljive za pomeranje lopte
+    double a, b;
+
 };
 
 #endif // LOPTAUUSTIMA_H
+
